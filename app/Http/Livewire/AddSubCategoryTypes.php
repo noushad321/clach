982b425a7d7032cfc47678b4c826d993ac655bd3
princia <princia23mendonca@gmail.com>
2022-12-02 10:24:21 +0400

    multimedia support for sub cat types

---
+++ app/Http/Livewire/AddSubCategoryTypes.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Category;
+use App\Models\Multimedia;
+use App\Models\SubCategory;
+use App\Models\SubCategoryType;
+use Illuminate\Support\Str;
+use Livewire\Component;
+use Livewire\WithFileUploads;
+
+class AddSubCategoryTypes extends Component
+{
+    use WithFileUploads;
+    public SubCategoryType $subCategoryType;
+    public $subCategory;
+    public $image;
+
+    protected $rules = [
+        'subCategoryType.name' => 'required|string',
+        'subCategoryType.description' => 'nullable|string',
+        'subCategory' => 'required|numeric',
+        'image' => 'image|max:1024',
+    ];
+
+    public function mount()
+    {
+        $this->subCategoryType = new SubCategoryType();
+        $this->subCategories = SubCategory::all();
+    }
+
+    public function save()
+    {
+        $this->validate();
+        $this->subCategoryType->slug = Str::slug($this->subCategoryType->name, '-');
+        $this->subCategoryType->fk_sub_category_id = $this->subCategory;
+        $this->subCategoryType->save();
+
+        $filePath = ( 'uploads/') . date('Y/m');
+        $file = $this->image;
+        $extension = $file->getClientOriginalExtension();
+        $fileName = uniqid() . '.' . $extension;
+        $uploadPath = $file->storeAs($filePath, $fileName);
+        $originalName = $file->getClientOriginalName();
+        $media = new Multimedia();
+        $media->name = $originalName;
+        $media->source_path= $uploadPath;
+        $media->order = 1;
+        $this->subCategoryType->multimedia()->save($media);
+
+        return redirect()->to('/sub-categories-types');
+    }
+    public function render()
+    {
+        return view('livewire.add-sub-category-types');
+    }
+}
--- resources/views/livewire/add-sub-category-types.blade.php
+++ resources/views/livewire/add-sub-category-types.blade.php
@@ -2,37 +2,44 @@
     <div class="px-4 sm:px-6 lg:px-8">
         <form wire:submit.prevent="save">
             <div class="mb-6">
-                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Sub Category Name</label>
+                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Sub Category Type Name</label>
                 <input type="text"
-                       wire:model="subCategory.name"
+                       wire:model="subCategoryType.name"
                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                        placeholder="name"
                        required="">
-                @error('subCategory.name') <span class="text-sm text-red-500">{{ $message }}</span> @enderror
+                @error('subCategoryType.name') <span class="text-sm text-red-500">{{ $message }}</span> @enderror
             </div>
             <div class="mb-6">
-                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Sub Category Description</label>
+                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Sub Category Type Description</label>
                 <input type="text"
-                       wire:model="subCategory.description"
+                       wire:model="subCategoryType.description"
                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                        placeholder="desc">
-                @error('subCategory.description') <span class="text-sm text-red-500">{{ $message }}</span> @enderror
+                @error('subCategoryType.description') <span class="text-sm text-red-500">{{ $message }}</span> @enderror
             </div>
-            @if($category)
+            @if($subCategories)
                 <div class="mb-6">
-                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Select Category</label>
-                    <select class="form-control mr-sm-2"  wire:model.defer="category">
-                        <option value=''>Select Category</option>
-                        @foreach ($category as $cat)
-                            <option value="{{$cat->id}}" > {{$cat->name}}</option>
+                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Select Sub Category</label>
+                    <select class="form-control mr-sm-2"  wire:model.defer="subCategory">
+                        <option value=''>Select Sub Category</option>
+                        @foreach ($subCategories as $subcat)
+                            <option value="{{$subcat->id}}" > {{$subcat->name}}</option>
                         @endforeach
                     </select>
-                    @error('product.category') <span class="text-sm text-red-500">{{ $message }}</span> @enderror
+                    @error('subCategory') <span class="text-sm text-red-500">{{ $message }}</span> @enderror
                 </div>
             @endif
+
+            <div class="mb-6">
+                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Image</label>
+                <input type="file" wire:model.defer="image">
+                @error('image') <span class="text-sm text-red-500">{{ $message }}</span> @enderror
+            </div>
+
             <div class="flex items-center justify-start space-x-4">
                 <div class="ml-12 sm:flex-right">
-                    <a href="/sub-categories"
+                    <a href="/sub-categories-types"
                        class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">
                         Back
                     </a>
